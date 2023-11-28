2c48bd1b38e3257ea7bd475518a450989d2284c0
Sarah Ogunko <sarahogunko@hotmail.com>
2023-11-28 18:03:05 +0000

    finished MVP

--- index.js
+++ index.js
@@ -1,19 +1,29 @@
 
 const getCountryByName = async(countryName)=>{
-  const response = await fetch ("https://restcountries.com/v3.1/name/" + countryName)
+  const response = await fetch (`https://restcountries.com/v3.1/name/${countryName}`)
   const data = await response.json();
-
   console.log(data)
+  displayInfo(data)
+}
+  
+const section = document.querySelector('#header-section')
+const displayInfo = (data) => {
+    const countryName = document.createElement("p");
+    countryName.innerText = data[0].name.common
+    section.appendChild(countryName)
+
+    const countryCapital = document.createElement("p");
+    countryCapital.innerText = data[0].capital[0]
+    section.appendChild(countryCapital)
+
+    const countryPopulation = document.createElement("p");
+    countryPopulation.innerText = data[0].population
+    section.appendChild(countryPopulation)
+
+    const countryLanguage = document.createElement("p");
+    countryLanguage.innerText = data[0].languages[0]
+    section.appendChild(countryLanguage)
 
 }
 
-getCountryByName("Peru")
-
-// const section = document.querySelector("#header-section")
-  
-//   data.forEach((country)=> {
-//     const countryInfo = document.createElement("p");
-//     countryInfo.textContent = country.name.common;
-//     section.appendChild(countryInfo)
-
-//   })
+getCountryByName("Christmas Island")
